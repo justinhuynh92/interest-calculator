a60a366fda9f8248988f242b70bab06b1d6bb77b
Justin Huynh <justinhuynh639@gmail.com>
2023-09-26 12:35:30 -0700

    printed monthly payment

--- app.py
+++ app.py
@@ -16,3 +16,7 @@ def main():
     amount_of_months = years * 12
     #calculate monthly payments
     monthly_payment = principal * monthly_interest_rate / (1 - (1 + monthly_interest_rate) ** (-amount_of_months))
+
+    print('The monthly payment for this lone is: ' + str(monthly_payment))
+
+main()
